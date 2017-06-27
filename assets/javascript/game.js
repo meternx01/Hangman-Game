589ff3a895db7b485ebefa1c5159799680b7fa23
Jason McKinney <meter_1@yahoo.com>
2017-06-27 14:37:06 -0500

    pseudo for game.js
    pseudo for game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+/* Variables to be used,
+Arrays: 
+Target words
+Target Word (Chars) 
+Current Board (chars)
+
+Numbers:
+guessesRemaining
+wins
+
+Functions:
+Start Game
+Pick Random Word
+onButtonUp Crap
+ifCorrect
+ifInCorrect
+youWin
+youFail
+*/
+
+//pickRandomWord returns an array of characters for a word chosen from a list of words
+function pickRandomWord()
+{
+	var possibleWords[""]
+
+}
